30360d7122b74de183d0154354831be0bd8cd26e
kamalafidele <kamalafizzet45@gmail.com>
2022-08-30 10:02:03 +0200

    added creation and updation functionality

--- store/serializers.py
+++ store/serializers.py
@@ -7,15 +7,37 @@ class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
 
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price','price_with_tax', 'collection']
+        fields = ['id', 'title', 'description','slug', 'inventory', 'price','price_with_tax', 'collection']
         
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = CollectionSerializer()
+    # collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.2)
+
+    #---------------- Custom validation Errors ----------------------
+    
+    # def validate(self, data):
+    #     if data['password'] != data['confirm_password']:
+    #         return serializers.ValidationError('Passwords do not match')
+    #     return data
+
+
+    # -------------- Overriding the Create method -------------------
+    
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+
+    # -------------- Overriding Update method -----------------------
+    
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get('unit_price')
+    #     instance.save()
+    #     return instance

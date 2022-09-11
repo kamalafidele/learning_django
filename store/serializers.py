d0cfc3df9a4cf212393017b41ac8e62c261e9278
kamalafidele <kamalafizzet45@gmail.com>
2022-09-11 08:23:27 +0200

    added orders api

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,7 @@
 from dataclasses import fields
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Cart, CartItem, Customer, Product, Collection, Review
+from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -119,3 +119,17 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product' ,'unit_price', 'quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = ['id', 'customer_id', 'placed_at', 'payment_status', 'items']
